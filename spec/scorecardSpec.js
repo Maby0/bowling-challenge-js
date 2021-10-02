4e672bdc198552713ebb877c06ddf16c95a46f3d
Mabon ap Gwyn <mabonapgwyn@hotmail.co.uk>
2021-10-02 17:58:45 +0100

    just .bowl method left to set up for scorecard

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -5,7 +5,7 @@ describe('Scorecard', () => {
     beforeEach(() => {
       scorecard = new Scorecard("Mabon");
     })
-    
+
     it('has a name value upon initialisation', () => {
       expect(scorecard.name).toEqual("Mabon");
     })
@@ -15,10 +15,40 @@ describe('Scorecard', () => {
     })
   })
 
+  describe('score', () => {
+    it('returns total running score', () => {
+      frame = new Frame;
+      frame.frameScore = 5;
+      scorecard = new Scorecard("Mabon");
+      scorecard.frames.push(frame);
+      expect(scorecard.score()).toEqual(5);
+    })
+  })
+
+  describe('nextFrame', () => {
+    it('adds a frame to frames property', () => {
+      scorecard = new Scorecard("Mabon");
+      scorecard.nextFrame();
+      expect(scorecard.frames.length).toEqual(1);
+    })
+  })
+
+  describe('framesPlayer', () => {
+    it('returns the number of frames played', () => {
+      scorecard = new Scorecard("Mabon");
+      for (let i = 0; i < 4; i++) {
+        scorecard.nextFrame();
+      }
+      expect(scorecard.framesPlayed()).toEqual(4);
+    })
+  })
+
   describe('.startGame / .currentGame', () => {
     it('.startGame creates a new instance of Scorecard which is accessed via .currentGame', () => {
       Scorecard.startGame("Mabon")
       expect(Scorecard.currentGame).toBeInstanceOf(Scorecard);
+      expect(Scorecard.currentGame.name).toEqual("Mabon");
+      expect(Scorecard.currentGame.frames.length).toEqual(1);
     })
   })
 })
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,12 +1,30 @@
 class Scorecard {
   static startGame(name) {
     this.currentGame = new Scorecard(name);
+    this.currentGame.frames.push(new Frame);
   }
 
   static currentGame;
 
+  static bowl() {
+    
+  }
+
   constructor(name) {
     this.name = name
     this.frames = []
+  }
+
+  score() {
+    let frameScoring = this.frames.map(frame => frame.frameScore) 
+    return frameScoring.reduce((a, b) => a + b);
+  }
+
+  nextFrame() {
+    this.frames.push(new Frame);
+  }
+
+  framesPlayed() {
+    return this.frames.length;
   }
 }
