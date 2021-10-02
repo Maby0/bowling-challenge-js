0fa2325fa936dd85b2c2d54b780371ce4d4a9f12
Mabon ap Gwyn <mabonapgwyn@hotmail.co.uk>
2021-10-02 13:19:03 +0100

    added static variable and method to store game instance

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -1,17 +1,24 @@
 describe('Scorecard', () => {
   let scorecard;
 
-  beforeEach(() => {
-    scorecard = new Scorecard("Mabon");
-  })
-
-  describe ('constructor', () => {
+  describe('constructor', () => {
+    beforeEach(() => {
+      scorecard = new Scorecard("Mabon");
+    })
+    
     it('has a name value upon initialisation', () => {
       expect(scorecard.name).toEqual("Mabon");
     })
 
     it('has an empty array of frames upon initialisation', () => {
       expect(scorecard.frames).toEqual([])
+    })
+  })
+
+  describe('.startGame / .currentGame', () => {
+    it('.startGame creates a new instance of Scorecard which is accessed via .currentGame', () => {
+      Scorecard.startGame("Mabon")
+      expect(Scorecard.currentGame).toBeInstanceOf(Scorecard);
     })
   })
 })
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,4 +1,10 @@
 class Scorecard {
+  static startGame(name) {
+    this.currentGame = new Scorecard(name);
+  }
+
+  static currentGame;
+
   constructor(name) {
     this.name = name
     this.frames = []
